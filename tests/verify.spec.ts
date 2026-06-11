7807e15b0601e0290afecacef45ee88a449452fd
Teemu H <44917122+swd1tn002@users.noreply.github.com>
2026-06-11 12:46:26 +0000

    Changed verification tests to ignore working directories and details in stack traces that break snapshots tests across environments

--- tests/verify.spec.ts
+++ tests/verify.spec.ts
@@ -12,15 +12,48 @@ beforeAll(async () => {
 
 describe('release-notes.md', () => {
     it('matches snapshot', () => {
-        expect(releaseNotes).toMatchSnapshot();
+        const contents = replaceVariableData(releaseNotes);
+
+        expect(contents).toMatchSnapshot();
     });
 });
 
 describe('result.json', () => {
     it('matches snapshot', () => {
         const resultJson = JSON.parse(resultJsonRaw);
+
+        // ignore datetime field in snapshot tests since it will be different on every test run
         resultJson["datetime"] = expect.any(String);
 
         expect(resultJson).toMatchSnapshot();
     });
 });
+
+/**
+ * Replaces variable data, such as working directory paths and line
+ * numbers in stack traces with placeholders so snapshots are less flaky
+ * across different environments, test runs and dependency versions.
+ */
+function replaceVariableData(report: string): string {
+    return ignoreErrorLineDetails(ignoreWorkingDirectory(report));
+}
+
+/**
+ * Working directory paths can be different across environments and runs, so this function
+ * replaces all occurrences of the current working directory in the given report with
+ * a placeholder string.
+ *
+ * This is useful for making test snapshots more stable and not dependent on the specific
+ * environment where the tests are run.
+ */
+function ignoreWorkingDirectory(report: string): string {
+    return report.replaceAll(process.cwd(), '<WORKING_DIRECTORY>');
+}
+
+function ignoreErrorLineDetails(report: string): string {
+    // find file paths followed by line numbers in error stack traces and replace them:
+    const errorLinePattern = /at file:[\S]+:\d+:\d+/g;
+
+    return report.replace(errorLinePattern, 'at file://<ERROR_LINE_DETAILS>');
+}
+
